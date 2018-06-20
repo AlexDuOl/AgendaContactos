3c8f2d89f0431328b8f213c31b978b9658a78e84
AlexDuOl <contactogatovagabundo@gmail.com>
2018-06-20 14:40:39 -0500

    Subiendo cambios en js para descontar targetas

--- js/app.js
+++ js/app.js
@@ -14,9 +14,9 @@ var template = '<div class="row">'+
                     '<div class="col offset-s3 s6">'+
                         '<div class="card blue-grey darken-1">'+
                             '<div class="card-content white-text">'+
-                            '<h3>__name__</h3>'+
-                            '<span>__phone__</span>'+
-                            '<a class="waves-effect waves-light btn right"><i class="material-icons center">delete</i></a>'+
+                              '<h3>__name__</h3>'+
+                              '<span>__phone__</span>'+
+                              '<a class="waves-effect waves-light btn right" onClick="deleteContact()"><i class="material-icons center">delete</i></a>'+
                             '</div>'+
                         '</div>'+
                     '</div>'+
@@ -31,6 +31,16 @@ function addContactData (name, phone) {
     $('h5').html('Total contactos: '+ count);
     swal("Yeeeii!", "Contacto agregado!", "success");
 };
+
+function deleteContact () {
+    count--;
+    var item = $(event.currentTarget);
+    var card = item.parent();
+    var colCard = card.parent();
+    var rowCard = colCard.parent();
+    rowCard.remove();
+    $('h5').html('Total contactos: ' + count);
+}
 
 
 
