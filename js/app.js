1d6aaf4815f0a250ba6aa3d2887f7dcb83f65b03
AlexDuOl <contactogatovagabundo@gmail.com>
2018-06-20 13:44:43 -0500

    Subiendo archivos

---
+++ js/app.js
@@ -0,0 +1,60 @@
+var count = 0;
+
+function getContactData () {
+    var $name = $('#contact-name');
+    var $phone = $('#contact-phone');
+    addContactData($name.val(), $phone.val());
+    $name.val('');
+    $phone.val('');
+   /* $('#contact-name').val('');*/
+   /* $('#contact-phone').val('');*/
+};
+
+var template = '<div class="row">'+
+                    '<div class="col offset-s3 s6">'+
+                        '<div class="card blue-grey darken-1">'+
+                            '<div class="card-content white-text">'+
+                            '<h3>__name__</h3>'+
+                            '<span>__phone__</span>'+
+                            '<a class="waves-effect waves-light btn right"><i class="material-icons center">delete</i></a>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>';
+
+function addContactData (name, phone) {
+    count++;
+    var finalTemplate = "";
+    finalTemplate = template.replace('__name__', name).replace('__phone__', phone);
+
+    $('main').append(finalTemplate);
+    $('h5').html('Total contactos: '+ count);
+    swal("Yeeeii!", "Contacto agregado!", "success");
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$(document).ready(function(){
+    $('.modal').modal();
+    $('#add-contact').click(getContactData)
+  });
+
+
+
+
